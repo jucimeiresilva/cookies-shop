73460ddfe432be5a247a6666e26425f0d4b6c94c
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-05-05 20:30:24 -0300

    teste heroku

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/user.model');
+const User = require('../models/User.model');
 
 const router = Router();
 
